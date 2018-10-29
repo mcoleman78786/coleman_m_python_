b86a5bd244b37383b21a516a69e5fc7055b94ad0
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2018-10-29 12:49:12 -0400

    added RPSGame.py

--- RPSGame.py
+++ RPSGame.py
@@ -1,12 +1,53 @@
-# import the random package so that we can generate random numbers
 from random import randint
 
-# choices is an array => a container that can hold multiple items
-# arrays are 0-based -> the first item is 0, the second is 1, etc
-choices= ["Rock", "Paper", "Scissors"]
+# available weapons=> store this in an array
+choices = ["Rock", "Paper", "Scissors"]
+player = False
 
-# make the computer choose a weapon from the choices array at random
-computer_choice= choices[randint(0,2)]
+# make the computer pick one item at random
+computer_choice = choices[randint(0, 2)]
 
-# print the choice to the terminal window
-print("computer_choices: ", computer_choice)
+# show the computer's choice in the terminal window
+print("computer_choices:", computer_choice)
+
+# set up our loop
+while player is False:
+    # set player to True by making a selection
+    print("Choose your weapon!")
+    input("Rock, Paper or Scissors?")
+
+    print(player, "\n")
+
+    # check for a tie = choices are the same
+    if player == computer_choice:
+        print("Tie! You live to shoot another day")
+
+    # check to see if the computer choice beats our choice or not
+    elif player == "Rock":
+        if computer_choice == "Paper":
+            # computer won. crap!!
+            print("You lose! Paper covers Rock")
+        else:
+            # we win! such winning
+            print("You win!", player, "smashes", computer_choice)
+
+    elif player == "Paper":
+        if computer_choice == "Scissors":
+            print("You lose!", computer_choice, "cut", player)
+        else:
+            print("You won!", player, "covers", computer_choice)
+
+    elif player == "Scissors":
+        if computer_choice == "Rock":
+            print("You lose!", computer_choice, "smashes", player)
+        else:
+            print("You win!", player, "cut", computer_choice)
+    elif player == "quit":
+        exit()
+    else:
+        print("Check your spelling... that's not a valid choice\n")
+
+# reset the game loop and start over again
+    player = False
+    computer_choice = choices[randint(0, 2)]
+
