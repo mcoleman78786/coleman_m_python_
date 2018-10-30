557643aca4f602ad993b2b9da2982dcbd253f461
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2018-10-30 13:18:15 -0400

    updated function.py

--- function.py
+++ function.py
@@ -1,16 +1,37 @@
 def greetings():
-    print("Hello from the greetings function"):
+    print("Hello from the greetings function")
 
 
-#the greeting function just says hello
-#invoke or call the function
+# the greeting function just says hello
+# invoke or call the function
 greetings()
 
 
-def greetingsWithArgs(msg):
+def greetingsWithArgs(msg="a deafult message"):
     print("now we're saying", msg, "from another function")
 
 
-greetingsWithArg("hey! sup!")
+greetingsWithArgs("")
 greetingsWithArgs("something competely different")
 greetingsWithArgs("running yet again")
+
+# varible and scope
+myNumberVariable = 10
+
+
+# returning values from functions (very powerful)
+def someMath(num1=2, num2=4):
+    global myNumberVariable
+
+    myNumberVariable = num1 + num2
+    return num1 + num2
+
+
+sum = someMath()
+print("we are doing some math and getting", sum, "as the result")
+
+sum = someMath(10, 15)
+print("another math operation with", sum, "as the result")
+
+
+# varible and scope
