5135b5475c21f7d9c665b5c2d01131d8d9e9b1de
Mallory Coleman <mallorycoleman@Mallorys-Air.fc.ca>
2018-10-30 12:34:43 -0400

    create new file called function.py and updated it

---
+++ function.py
@@ -0,0 +1,16 @@
+def greetings():
+    print("Hello from the greetings function"):
+
+
+#the greeting function just says hello
+#invoke or call the function
+greetings()
+
+
+def greetingsWithArgs(msg):
+    print("now we're saying", msg, "from another function")
+
+
+greetingsWithArg("hey! sup!")
+greetingsWithArgs("something competely different")
+greetingsWithArgs("running yet again")
