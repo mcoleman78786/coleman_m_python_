ead02b9d571a715bab13f7ae556d75e0bf502ff4
Mallory Coleman <mallorycoleman@Mallorys-MacBook-Air.local>
2018-10-31 13:15:15 -0400

    updated RPSGame.py

--- RPSGame.py
+++ RPSGame.py
@@ -1,70 +1,100 @@
+# import the random packages so that we can generate random numbers
 from random import randint
 
-# available weapons=> store this in an array
+# choices is an array => a container that can hold multiple items
+# # arrays are a 0-based -> the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
 player = False
 
-player_lives = 5
-computer_lives = 5
+player_lives = 3
 
-# make the computer pick one item at random
-computer_choice = choices[randint(0, 2)]
+# make the computer choose a weapon from choices at random
+computer_choice = choices[randint(0,2)]
 
+computer_choice_lives = 3
 
-# set lives for computer and player
-player = 3
-computer_choice = 3
-
-player = False
-
-# show the computer's choice in the terminal window
-print("computer_choices:", computer_choice)
+# print the choice to the terminal window
+#print("Computer chooses: ", computer_choice)
 
 # set up our loop
 while player is False:
-    print("============================================")
-    print("Player lives:", player_lives, "/5")
-    print("computer_lives lives:", computer_lives, "/5")
-    print("============================================")
-    # set player to True by making a selection
-    print("Choose your weapon!")
-    input("Rock, Paper or Scissors?")
+	#set player to True by making a selection
+	print("=====================================")
+	print("Choose your weapon!")
+	player = input("Rock, Paper or Scissors?\n")
+
+	print("Player chooses", player, "\n")
+	print("Computer chooses: ", computer_choice)
+
+	if player == computer_choice:
+		print("Tie! You live to shoot another day")
+		print("Player Score", player_lives,"Computer Score", computer_choice_lives)
 
 
-    print(player, "\n")
 
-    # check for a tie = choices are the same
-    if player == computer_choice:
-        print("Tie! You live to shoot another day")
+	elif player == "Rock":
+		if computer_choice == "Paper":
+			print("You lose! Paper covers Rock")
+			player_lives = player_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
+		
+		else:
+			print("You win!", player, "smashes", computer_choice)
+			computer_choice_lives = computer_choice_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
+			
 
-    # check to see if the computer choice beats our choice or not
-    elif player == "Rock":
-        if computer_choice == "Paper":
-            # computer won. crap!!
-            player_lives = player_lives -1
-            print("You lose! Paper covers Rock")
-        else:
-            # we win! such winning
-            print("You win!", player, "smashes", computer_choice)
+	elif player == "Paper":
+		if computer_choice == "Scissors":
+			print("You lose! Scissors cuts Paper")
+			player_lives = player_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
+		else:
+			print("You win!", player, "covers", computer_choice)
+			computer_choice_lives = computer_choice_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
 
-    elif player == "Paper":
-        if computer_choice == "Scissors":
-        	 player_lives = player_lives -1
-            print("You lose!", computer_choice, "cut", player)
-        else:
-            print("You won!", player, "covers", computer_choice)
+	elif player == "Scissors":
+		if computer_choice == "Rock":
+			print("You lose! Rock crushes Scissors")
+			player_lives = player_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
+		else:
+			print("You win!", player, "cuts", computer_choice)
+			computer_choice_lives = computer_choice_lives - 1
+			print("Player Score", player_lives,"Computer Score", computer_choice_lives)
 
-    elif player == "Scissors":
-        if computer_choice == "Rock":
-        	 computer_lives = computer_lives -1 
-            print("You lose!", computer_choice, "smashes", player)
-        else:
-            print("You win!", player, "cuts", computer_choice)
-    elif player == "Quit":
-        exit()
-    else:
-        print(" Not a vlaid option..Check again, check your spelling\n")
+	elif player == "quit":
+			exit()
+	else:
+		print("Check your spelling... that's not a valid choice\n")
 
-# reset the game loop and start over again
-    player = False
-    computer_choice = choices[randint(0, 2)]
+	if computer_choice_lives < 1:
+		print("YOU WIN THE GAME")
+		choice = input("Enter 'Yes' to restart or 'No' to exit")
+		
+		if choice == "Yes":
+			player_lives = 3
+			computer_choice_lives = 3
+			player=False
+			computer_choice = choices[randint(0, 2)]
+
+		elif userInput == "No":
+			exit()
+
+	if player_lives < 1:
+		print("YOU LOSE THE GAME")
+		choice = input("Enter 'Yes' to restart or 'No' to exit")
+		if choice == "Yes":
+			player_lives = 3
+			computer_choice_lives = 3
+			player=False
+			computer_choice = choices[randint(0, 2)]
+
+		elif choice == "No":
+			exit()
+
+		
+	player = False 
+	computer_choice = choices[randint(0, 2)]
+
